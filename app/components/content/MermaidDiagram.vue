771f81d65010ec7ca33ded991444bb65a071f13e
daiv05 <davidderas50@gmail.com>
2026-03-30 10:53:47 -0600

    chore: add vue-mermaid-render dependency to package.json

---
+++ app/components/content/MermaidDiagram.vue
@@ -0,0 +1,22 @@
+<template>
+    <VueMermaidRender :content="content" :config="config" ></VueMermaidRender>
+</template>
+<script setup>
+import { VueMermaidRender } from 'vue-mermaid-render'
+const props = defineProps({
+    content: {
+        type: String,
+        required: true
+    },
+    config: {
+        type: Object,
+        default: () => ({
+                theme: 'neutral',
+                flowchart: {
+                    useMaxWidth: true,
+                    htmlLabels: true
+                }
+        })
+    }
+})
+</script>
